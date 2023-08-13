b9b61da93e4cf87e05593be76a27bbfa8505c0cb
Alim <alim-malikov@mail.ru>
2023-08-13 11:06:59 +0500

    second commit

--- Task №30/main.py
+++ Task №30/main.py
@@ -12,3 +12,34 @@ n = int(input("Введите число членов арифметическо
 for i in range(n):
     A_n= A_1 + i * d
     print(A_n)
+
+
+"""
+Задача 32: Определить индексы элементов массива (списка), 
+значения которых принадлежат заданному диапазону
+ (т.е. не меньше заданного минимума и не больше заданного максимума)
+"""
+
+
+from random import randint
+
+def Bubble_sorting_list(rand_list):
+    for i in rand_list(len(rand_list)-1):
+        for j in rand_list(len(rand_list)-i-1):
+            if rand_list[j] > rand_list[j + 1]:
+                rand_list[j], rand_list[j + 1] = rand_list[j + 1], rand_list[j]
+                print(rand_list, 'end=')
+
+
+#
+# lower=int(input("Введите нижнюю границу рандомайзера: "))
+# upper=int(input("Введите верхнюю границу рандомайзера: "))
+# rand_list=[]
+#
+# n=int(input("Введите длину списка из случайных чисел: "))
+#
+# for _ in range(n):
+#     rand_list.append(randint(lower,upper))
+#     print(rand_list)
+#
+# Bubble_sorting_list(rand_list)
